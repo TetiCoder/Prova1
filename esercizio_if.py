cf2f4ee7d789d59bf24df8f102cef0f1172f9f7f
TetiCoder <teticoder@MacBook-Pro.local>
2018-05-04 20:35:58 -0700

    aggiungo esecizio per IF

---
+++ esercizio_if.py
@@ -0,0 +1,10 @@
+"""
+esercizio per imparare l'IF
+"""
+print("Hello World!")
+NOME = input("come ti chiami?")
+if NOME == "Carlo":
+    print("Benvenuto mio padrone!")
+else:
+    print("Arrestate questo impostore!")
+print("The End!")
